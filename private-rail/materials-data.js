7070f0270329ff7771c7fda59e258b9af541d4bd
mj880616 <mj880616@gmail.com>
2026-09-11 04:26:49 +0900

    Refactor private rail materials data

---
+++ private-rail/materials-data.js
@@ -0,0 +1,31 @@
+window.PRIVATE_RAIL_MATERIALS = [
+  {
+    no: '01',
+    name: '정책 · 연구 · 토론회',
+    folder: 'https://drive.google.com/drive/folders/1QWgdbnPuETMH2BCwpGuat8rRoRSJGPLy',
+    files: [
+      ['1dG1NH_1kXbhVzK4V1e0KGTaxhGMzLVMg','2026 철도 구조개혁을 넘어 모달시프트로 2부','PDF'],
+      ['16T_imtKeoIyLWFbSZY1C5Zhcf_nMdGcM','민자철도시대: 보장된 이익, 관성적 제도, 그리고 정부의 역할','PDF · 보고서'],
+      ['1C9boWD6VUiawVVSKtQxOwBFPIzVVleN1','PPIP WP 기존 철도망 계획 비판과 대안 철도망 개념 제안(전현우)','PDF'],
+      ['1BXQg7Vub_n65har9ragP-85TYY_9J_Cz','PPIP WP 지속가능하고 안정적인 철도 예산, 현황과 대안(김상철)','PDF'],
+      ['18R9plJBkd-UAJzUvrYRMl-Hzr1myTLpm','PPIP WP 민자철도사업 운영구조 분석과 문제점 비판(이영수)','PDF'],
+      ['1yAUgc5-SYcutlgp8RbYFRU_wx9UEASvZ','2026 철도 구조개혁을 넘어 모달시프트로 3부','PDF'],
+      ['1IlFME7XuFXZW4bfiZ-TA6zWt5RwR8S7l','2026 철도 구조개혁을 넘어 모달시프트로 1부','PDF'],
+      ['1oK-JuRcBkgb1OFLhDeEC-hHDU3zKDLwC','2024 공공철도지하철정책대회 자료집 현장세션3','PDF'],
+      ['1ip8TB4vdsbVYU3RxSqDOK8jC17s23XMJ','251112 민간도시철도 공공성 강화 도시철도법 개선 방향 국회토론회 자료집','PDF'],
+      ['1FlqLwk14n8VAsWTwXBADboK7i1Ugy2zm','민간도시철도의 사회적 규제와 공영화 추진 방향 최종보고서','PDF · 보고서'],
+      ['1GsjGIZCitoVH5bxW7qHZ8m6srnclkorP','2024 공공철도지하철정책대회 자료집 종합세션1-2','PDF']
+    ]
+  },
+  { no:'02', name:'국회 대응', folder:'https://drive.google.com/drive/folders/1zE7fdznyhUjGC7eG3YTA4oEVksTGOW-l', files:[] },
+  {
+    no:'03', name:'국토교통부 대응', folder:'https://drive.google.com/drive/folders/1FlvAu9QiR5gkgPllnbEhJXRHAmaHGrCr',
+    files:[
+      ['1keBLcRaSZKwdqc0R8eZcHXQsdQir-CQ2','260625 민자철도 정책 및 제도개선요구','PDF'],
+      ['1Rnda1_SlM49jv29LUFzvav2pvrcw2SDw','260731 민자철도 운영실태 개선요구(안)','PDF']
+    ]
+  },
+  { no:'04', name:'현장실태 · 사업장자료', folder:'https://drive.google.com/drive/folders/16sDxqEjrL0P-Qz4kM_eZ9eN_UvXHTWDr', files:[] },
+  { no:'05', name:'성명 · 보도자료', folder:'https://drive.google.com/drive/folders/1dGM_W5dxIh2cXno-EijrkP67IImSPl2g', files:[] },
+  { no:'06', name:'교육 · 선전자료', folder:'https://drive.google.com/drive/folders/10uLoVGSzNhYcph7t2Q2BE0XAE_HxsAVH', files:[] }
+];
