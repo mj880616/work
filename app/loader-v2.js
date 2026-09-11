0d7f5d40ac530bc7c25001f4f29cfd789072c234
mj880616 <mj880616@gmail.com>
2026-09-12 06:33:44 +0900

    Load project file workflow

--- app/loader-v2.js
+++ app/loader-v2.js
@@ -11,7 +11,8 @@
     import('./password-reset.js?v=1'),
     import('./auth-ui.js?v=3'),
     import('./calendar-health.js?v=1'),
-    import('./task-workflow.js?v=1')
+    import('./task-workflow.js?v=1'),
+    import('./project-files.js?v=1')
   ]);
   await import('./auth-cleanup.js?v=1');
 })().catch(err=>{
