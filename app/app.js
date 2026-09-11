de9399aebf6a8d09b9ca5b35eff2434de72e255e
mj880616 <mj880616@gmail.com>
2026-09-11 22:38:36 +0900

    Enable Windows install support

--- app/app.js
+++ app/app.js
@@ -1,1 +1,2 @@
+import('./pwa.js?v=1');
 import('./loader-v2.js?v=1').catch(err=>{console.error(err);document.body.insertAdjacentHTML('beforeend','<pre style="padding:16px;color:#a33b45">앱 초기화 오류: '+String(err.message||err)+'</pre>')});
