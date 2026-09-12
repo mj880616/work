01448c40c0fe7e0e4e9591478c91dec990c39c3f
mj880616 <mj880616@gmail.com>
2026-09-13 06:14:16 +0900

    프로젝트 v2 허브를 앱 로더에 연결

--- app/loader-v2.js
+++ app/loader-v2.js
@@ -41,6 +41,7 @@
   await import('./task-project-routing.js?v=1');
   await import('./project-update-actions.js?v=3');
   await import('./project-task-link.js?v=5');
+  await import('./project-v2.js?v=1');
   await import('./project-task-guide-cleanup.js?v=1');
   await import('./task-completed-label.js?v=2');
   await import('./task-notes.js?v=2');
