f5624748e0c341f7512f02fb753a46eb28abd02d
mj880616 <mj880616@gmail.com>
2026-09-12 20:41:04 +0900

    Load fixed project file uploader

--- app/loader-v2.js
+++ app/loader-v2.js
@@ -22,7 +22,7 @@
     import('./task-layout.js?v=2'),
     import('./profile-settings.js?v=1'),
     import('./meeting-round-detail.js?v=1'),
-    import('./project-files.js?v=1'),
+    import('./project-files.js?v=2'),
     import('./library-upload.js?v=1'),
     import('./page-editor-fix.js?v=1')
   ]);
