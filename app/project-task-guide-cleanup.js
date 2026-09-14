aec9db3d24448275598bf30957933cb2959c6938
mj880616 <mj880616@gmail.com>
2026-09-15 02:56:01 +0900

    불필요한 프로젝트 할 일 DOM 감시 잔재 제거

--- app/project-task-guide-cleanup.js
+++
@@ -1,6 +0,0 @@
-function removeProjectTaskGuide(){
-  document.querySelector('#projectTasksSection .pt-head p')?.remove();
-}
-removeProjectTaskGuide();
-const observer=new MutationObserver(removeProjectTaskGuide);
-observer.observe(document.body,{childList:true,subtree:true});
