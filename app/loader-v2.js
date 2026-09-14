8bc92df94ef5d2fb2663293a6fb0e0b53ce6ac5f
mj880616 <mj880616@gmail.com>
2026-09-15 01:22:26 +0900

    chore: bump meeting form contract cache version

--- app/loader-v2.js
+++ app/loader-v2.js
@@ -24,7 +24,7 @@
     return;
   }
 
-  await import('./meeting-multi-tasks.js?v=4');
+  await import('./meeting-multi-tasks.js?v=5');
   await import('./team.js?v=8');
   const waitForTeamState=async()=>{
     const rt=window.KPTURuntime;
