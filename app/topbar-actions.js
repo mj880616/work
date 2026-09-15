14e18a7c3c18427144d29caef92c8d2222cfd976
mj880616 <mj880616@gmail.com>
2026-09-15 17:48:25 +0900

    상단바 액션을 초기 단일 렌더러로 분리

---
+++ app/topbar-actions.js
@@ -0,0 +1,18 @@
+(()=>{
+'use strict';
+if(window.__KPTU_TOPBAR_ACTIONS__)return;
+window.__KPTU_TOPBAR_ACTIONS__=true;
+const host=document.querySelector('.top-actions');
+if(!host)return;
+let button=document.querySelector('#ccNotifTop');
+if(!button){
+  button=document.createElement('button');
+  button.id='ccNotifTop';
+  button.type='button';
+  button.className='ghost';
+  button.innerHTML='알림 <span id="ccNotifBadge" class="cc-count hidden">0</span>';
+  host.prepend(button);
+}
+button.classList.remove('hidden');
+button.onclick=()=>window.KPTURouter?.go?.('notifications',{source:'topbar'});
+})();
