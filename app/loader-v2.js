6f4eb112235420afc280a1bb86346a06160ed79f
mj880616 <mj880616@gmail.com>
2026-09-15 13:37:41 +0900

    Load library public visibility controls

--- app/loader-v2.js
+++ app/loader-v2.js
@@ -62,6 +62,7 @@
     import('./workplace-detail.js?v=2'),
     import('./project-files.js?v=4'),
     import('./library-upload.js?v=3'),
+    import('./library-public-toggle.js?v=1'),
     import('./page-editor-fix.js?v=2'),
     import('./page-builder.js?v=1&designer=2'),
     import('./page-shortcut.js?v=1'),
