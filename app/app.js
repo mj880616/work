37252d2ca1e9e785b8e21e66684b710a3c53f5a6
mj880616 <mj880616@gmail.com>
2026-09-23 08:26:18 +0900

    Refresh loader for Google OAuth return

--- app/app.js
+++ app/app.js
@@ -65,7 +65,7 @@
     window.dispatchEvent(new Event('kptu:app-ui-ready'));
     maybeDebugButton(sample);
   };
-  import('./loader-v2.js?v=190').catch(err=>{
+  import('./loader-v2.js?v=191').catch(err=>{
     console.error(err);startup.finalize('error');window.__KPTU_MARK_APP_UI_READY__?.();
     document.body.insertAdjacentHTML('beforeend','<pre style="padding:16px;color:#a33b45">앱 초기화 오류: '+String(err.message||err)+'</pre>');
   });
