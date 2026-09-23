4e32ea9f95a5de0259a9f06855e0235d47e7dc8d
mj880616 <mj880616@gmail.com>
2026-09-23 21:45:43 +0900

    Load refreshed feature styles

--- app/loader-v2.js
+++ app/loader-v2.js
@@ -71,7 +71,7 @@
   const ensureFeatureStyles=()=>featureStylesPromise||(featureStylesPromise=new Promise(resolve=>{
     if(document.querySelector('link[data-kptu-feature-styles]')){resolve();return}
     const link=document.createElement('link');
-    link.rel='stylesheet';link.href='./styles.css?v=41';link.dataset.kptuFeatureStyles='1';
+    link.rel='stylesheet';link.href='./styles.css?v=42';link.dataset.kptuFeatureStyles='1';
     link.onload=()=>resolve();link.onerror=()=>resolve();
     document.head.appendChild(link);
   }));
