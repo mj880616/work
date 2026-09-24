e64e4f9dc10bf4a67a84c4bbd1d8858a46f21722
mj880616 <mj880616@gmail.com>
2026-09-25 06:48:37 +0900

    Remove Web Push client bootstrap

--- app/loader-v2.js
+++ app/loader-v2.js
@@ -132,7 +132,6 @@
   window.__KPTU_MARK_APP_UI_READY__?.({usable:result?.ok===true});
   import('./mobile-modal-history.js?v=1').catch(()=>{});
   const defer=window.requestIdleCallback||((fn)=>setTimeout(fn,200));
-  defer(()=>import('./push-notifications-ui.js?v=4').catch(()=>{}),{timeout:2500});
   await mobileNavigationReady;
 })().catch(err=>{
   console.error(err);
