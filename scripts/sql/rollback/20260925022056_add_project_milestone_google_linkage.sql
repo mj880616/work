6a3a1e838503ae543984d8d5adaa0a4f43894e5d
mj880616 <mj880616@gmail.com>
2026-09-25 12:30:07 +0900

    Task 8: remove stale rollback filename

--- scripts/sql/rollback/20260925022056_add_project_milestone_google_linkage.sql
+++
@@ -1,6 +0,0 @@
-alter table public.app_project_milestones
-  drop constraint if exists app_project_milestones_google_linkage_pair_chk;
-
-alter table public.app_project_milestones
-  drop column if exists google_event_id,
-  drop column if exists google_calendar_id;
